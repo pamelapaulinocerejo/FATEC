4f397ad08fc2babfdd300bb20fb6a7e29b979032
pamelapaulinocerejo <31866367+pamelapaulinocerejo@users.noreply.github.com>
2018-03-19 11:50:39 -0300

    Update EX04_12_03.cpp

--- EX04_12_03.cpp
+++ EX04_12_03.cpp
@@ -1,9 +1,9 @@
-/*04) FaÁa um programa que receba o cÛdigo correspondente ao cargo de um funcion·rio e o
-seu sal·rio atual e, mostre o cargo, o valor do aumento e seu novo sal·rio. Os cargos s„o
+/*04) Fa√ßa um programa que receba o c√≥digo correspondente ao cargo de um funcion√°rio e o
+seu sal√°rio atual e, mostre o cargo, o valor do aumento e seu novo sal√°rio. Os cargos s√£o
 dados na tabela a seguir. Utilize switch.
 
-1  - Escritur·rio  - 50%
-2  - Secret·rio  - 35%
+1  - Escritur√°rio  - 50%
+2  - Secret√°rio  - 35%
 3  - Caixa  - 20%
 4  - Gerente  - 10%
 5  - Diretor  - 0% */
@@ -18,42 +18,50 @@ main(){
 	setlocale(LC_ALL, "Portuguese");
 	int cod;
 	float sal, nsal;
-	cout << "Digite o cÛdigo do funcion·rio: ";
+	cout << "[1] Escritur√°rio" << endl;
+	cout << "[2] Secret√°rio" << endl;
+	cout << "[3] Caixa" << endl;
+	cout << "[4] Gerente" << endl;
+	cout << "[5] Diretor" << endl;
+	
+	cout << "Digite o c√≥digo do funcion√°rio: ";
 	cin >> cod;
-	cout << "Digite o sal·rio atual do funcion·rio: R$ ";
+	cout << "Digite o sal√°rio atual do funcion√°rio: R$ ";
 	cin >> sal;
+	cout << "\n";
+	
 	switch(cod){
 		case 1:
-			cout << "Cargo: Escritur·rio" <<endl;
+			cout << "Cargo: Escritur√°rio" <<endl;
 			nsal = sal + (sal*0.5);
-			cout << "VocÍ tem direito a um aumento de 50%" << endl;
-			cout << "Novo sal·rio: R$" << nsal << endl;
+			cout << "Voc√™ tem direito a um aumento de 50%" << endl;
+			cout << "Novo sal√°rio: R$" << nsal << endl;
 			break;
 			
 		case 2:
-			cout << "Cargo: Secret·rio" <<endl;
+			cout << "Cargo: Secret√°rio" <<endl;
 			nsal = sal + (sal*0.35);
-			cout << "VocÍ tem direito a um aumento de 35%" << endl;
-			cout << "Novo sal·rio: R$" << nsal <<endl;
+			cout << "Voc√™ tem direito a um aumento de 35%" << endl;
+			cout << "Novo sal√°rio: R$" << nsal <<endl;
 			break;
 			
 		case 3:
 			cout << "Cargo: Caixa" << endl;
 			nsal = sal + (sal*0.2);
-			cout << "VocÍ tem direito a um aumento de 20%" << endl;
-			cout << "Novo sal·rio: R$ " << nsal << endl;
+			cout << "Voc√™ tem direito a um aumento de 20%" << endl;
+			cout << "Novo sal√°rio: R$ " << nsal << endl;
 			break;
 			
 		case 4: 
 			cout << "Cargo: Gerente" << endl;
 			nsal = sal + (sal*0.1);
-			cout << "VocÍ tem direito a um aumento de 10%" << endl;
-			cout << "Novo sal·rio: R$ " << nsal << endl;
+			cout << "Voc√™ tem direito a um aumento de 10%" << endl;
+			cout << "Novo sal√°rio: R$ " << nsal << endl;
 			break;
 			
 		default:
 			cout << "Cargo: Diretor" <<endl;
-			cout << "VocÍ N√O tem direito a um aumento" << endl;
+			cout << "Voc√™ N√ÉO tem direito a um aumento" << endl;
 			break;
 	
 	return 0;
